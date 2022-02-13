52b2912fb625dc4c13f21a3cfef3142e5032118c
Abdullah AL Shohag <HackerShohag@outlook.com>
2022-02-14 01:50:36 +0600

    added sortNumber function to sort only bangladeshi numbers

--- Tool for Merging Duplicate Contacts/MergeDuplicateContacts.py
+++ Tool for Merging Duplicate Contacts/MergeDuplicateContacts.py
@@ -1,9 +1,8 @@
 '''
 <no-license>
-A tool to make with VObject module to merge the duplicate contacts. It became handy.
+A tool to make with VObject module to merge the duplicate contacts. It became handy. The sortNumber function can perform the task to sort only the Bangladeshi numbers
 <feel free to use it>
 '''
-
 
 import vobject
 
@@ -18,32 +17,43 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-data = open("/home/shohag/Videos/ubuntu_contacts_20220203095752.vcf",'r').read()
+def sortNumber(number):
+    number = ''.join(str(v) for v in [int(word) for word in number if word.isdigit()])
+    if len(number) == 11:
+        number = number.replace("0",'',1)
+        number = "+880 " + number[:4] + "-" + number[4:]
+    if len(number) == 13:
+        number = "+" + number[:3] + " " + number[3:7] + "-" + number[7:]
+    return number
+
+
+data = open("/home/shohag/Videos/removedtags.vcf",'r').read()
 newVObject = vobject.vCard()
 
 nameList = []
 sortedContactsList = []
 
 for vcard in vobject.readComponents(data):
-    if vcard.fn.value in nameList:
-        index = nameList.index(vcard.fn.value)
+    if vcard.fn.value.lower() in nameList:
+        index = nameList.index(vcard.fn.value.lower())
         singleOldVObjectData = vobject.readOne(sortedContactsList[index])
         print(f"{bcolors.WARNING}{bcolors.BOLD}Dup: {bcolors.ENDC}{bcolors.WARNING}Duplicate contact, index: {index}, name: {vcard.fn.value}")
         newPhoneNumbers = vcard.contents['tel']
         for number in newPhoneNumbers:
             tel = singleOldVObjectData.add('tel')
             tel.type_param = "cell"
-            tel.value = number.value
-            print(f"\t{bcolors.WARNING}{bcolors.OKBLUE}DupInfo: {bcolors.ENDC}{bcolors.OKBLUE}added duplicate number {number.value}")
+            tel.value = sortNumber(number.value)
+            print(f"\t{bcolors.WARNING}{bcolors.OKBLUE}DupInfo: {bcolors.ENDC}{bcolors.OKBLUE}added duplicate number {tel.value}")
         sortedContactsList[index] = singleOldVObjectData.serialize()
     else:
+        vcard.tel.value = sortNumber(vcard.tel.value)
         print(f"{bcolors.OKGREEN}{bcolors.BOLD}AddInfo: {bcolors.ENDC}{bcolors.OKGREEN}added new contact {vcard.fn.value}, {vcard.tel.value}")
-        nameList.append(vcard.fn.value)
+        nameList.append(vcard.fn.value.lower())
         sortedContactsList.append(vcard.serialize())
 
 
-file = open("sortedContacts.vcf",'w')
-print(f"{bcolors.BOLD}{bcolors.HEADER}Info: {bcolors.ENDC}{bcolors.HEADER}Writting all the sorted data to the 'sortedContacts.vcf' file")
+file = open("sortedContactsWithRemovedTags.vcf",'w')
+print(f"{bcolors.BOLD}{bcolors.HEADER}Info: {bcolors.ENDC}{bcolors.HEADER}Writting all the sorted data to the 'sortedContactsWithRemovedTags.vcf' file")
 file.write(''.join(sortedContactsList))
 print(f"{bcolors.BOLD}{bcolors.HEADER}Info: {bcolors.ENDC}{bcolors.HEADER}write successful, closing now")
 file.close()
